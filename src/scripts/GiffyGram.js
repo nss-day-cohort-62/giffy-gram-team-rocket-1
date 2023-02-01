cc9257a629ddfae1dc9249995e2e8069a0ea7cbf
Vision Filler <visionfiller@gmail.com>
2023-02-01 13:59:17 -0600

    post

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,9 +1,43 @@
 import { Header } from "./nav/Header.js"
 import { Footer } from "./nav/Footer.js"
+import { createPost } from "./feed/PostList.js"
+
+
+
+
+document.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "postButton") {
+        renderPost()
+       /* applicationElement.addEventListener(
+            "stateChanged",
+            customEvent => {
+                renderApp()
+            }
+
+    */
+    }
+
+
+})
+
+
+
+export const renderPost = () => {
+    const newPost = document.querySelector(".postForm")
+    newPost.innerHTML = createPost()
+    //document.dispatchEvent( new CustomEvent("stateChanged"))
+}
+
+
+
+
 export const GiffyGram = () => {
 
     // Show main main UI
     return `<div class="navigation">${Header()}</div>
+    <div class="messages"></div>
+    <div class="postForm"><button id ="postButton">create post</button></div>
+    
     <footer>${Footer()}</footer>
     `
 }
