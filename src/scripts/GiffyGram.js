359411e05d6cef8c93d5a44a33d1147c0e23095d
Vision Filler <visionfiller@gmail.com>
2023-02-01 10:56:40 -0600

    first round of code

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,5 +1,20 @@
+
+const applicationElement= document.querySelector(".giffygram")
+applicationElement.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "logout") {
+        
+            localStorage.removeItem("gg_user") 
+            document.querySelector(".giffygram").dispatchEvent(new CustomEvent("stateChanged"))
+    }
+})
+
+
+
+
+
 export const GiffyGram = () => {
 
     // Show main main UI
-    return `<h1>Giffygram</h1>`
+    return `<h1>Giffygram</h1>
+        <button id ="logout">logout</button>`
 }
--- src/scripts/auth/Login.js
+++ src/scripts/auth/Login.js
@@ -1,4 +1,5 @@
 import { getUsers } from "../data/provider.js"
+import { Register} from "./Register.js"
 
 
 document.addEventListener("click", clickEvent => {
@@ -37,5 +38,9 @@ export const LoginForm = () => {
             </form>
             <button id="loginButton">Login</button>
         </div>
+
+
+
+        ${Register()}
     `
 }
